6bad702a180b7e2164d2eff2824697aac488ec3d
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2022-12-03 14:56:25 +0200

    Time: 1125 ms (17.20%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,10 +1,28 @@
 /* Write your T-SQL query statement below */
 
-select product_id ,'store1' as store  ,store1 as price  from products   where store1 is not null
--- order by product_id  
-union 
-select product_id ,'store2' as store  ,store2 as price  from products  where store2 is not null
--- order by product_id 
-union
-select product_id ,'store3' as store  ,store3 as price  from products where store3 is not null
-order by 1,2,3
+
+
+/************** using UNpivot ***********/
+
+
+
+select product_id , store  , price  
+
+from products 
+unpivot(
+    price for store in (store1,store2,store3)
+)
+as T
+
+
+
+
+
+
+/************** using UNION ***********/
+-- select product_id ,'store1' as store  ,store1 as price  from products   where store1 is not null
+-- union 
+-- select product_id ,'store2' as store  ,store2 as price  from products  where store2 is not null
+-- union
+-- select product_id ,'store3' as store  ,store3 as price  from products where store3 is not null
+-- order by 1,2,3
