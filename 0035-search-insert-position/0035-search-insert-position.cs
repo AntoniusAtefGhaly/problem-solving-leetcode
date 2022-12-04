9cd9b39a2e743fda6b2591a78c7fe30301a9539b
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2022-12-04 15:50:43 +0200

    Time: 134 ms (68.97%), Space: 37.9 MB (36.94%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+                int size=nums.Length;
+        int l=0,r=size-1;
+        int m=l+(r-l)/2;
+        
+        // if (size==1)
+        // {
+        //     if(nums[0]==target)
+        //         return 0;
+        // }
+        while(l<=r){
+            if(target==nums[m]){
+                return m;
+            }
+            else if(target>nums[m]){
+                
+                l=m+1;
+                m=l+(r-l)/2;
+                
+            }
+            else{
+                r=m-1;
+                m=l+(r-l)/2;
+            }
+        }
+      
+        return m;
+    }
+}
