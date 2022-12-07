7feba7694a270bc8fbd0183a67c8f74d76d5c28a
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2022-12-07 13:29:51 +0200

    Time: 464 ms (49.56%), Space: 47.7 MB (70.01%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cs
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -7,11 +7,12 @@ public class Solution {
         //     s[s.Length-1-i]=temp;
         // }
         
-         for (int i=0;i<(int)(s.Length/2) +s.Length%2 ;i++){
+        int m=(int)(s.Length/2) +s.Length%2;
+        
+         for (int i=0;i<m ;i++){
             char temp=s[i];
             s[i]=s[s.Length-1-i];
             s[s.Length-1-i]=temp;
         }
-        
     }
 }
