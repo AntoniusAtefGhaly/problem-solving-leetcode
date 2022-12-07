0d2709859f82ab6fc88c922d28c3e323848b629d
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2022-12-07 13:45:02 +0200

    Time: 491 ms (31.95%), Space: 47.6 MB (80.52%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cs
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -9,23 +9,23 @@ public class Solution {
         
 /**************sol 2****************/
 
-//         int m=(int)(s.Length/2) +s.Length%2;
+        int m=(int)(s.Length/2) +s.Length%2;
         
-//          for (int i=0;i<m ;i++){
-//             char temp=s[i];
-//             s[i]=s[s.Length-1-i];
-//             s[s.Length-1-i]=temp;
-//         }
+         for (int i=0;i<m ;i++){
+            char temp=s[i];
+            s[i]=s[s.Length-1-i];
+            s[s.Length-1-i]=temp;
+        }
         
  /**************sol 3****************/
-         int i = 0;
-        int j = s.Length - 1;
-        while (i < j) {
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-            i++;
-            j--;
-        }
+        //  int i = 0;
+        // int j = s.Length - 1;
+        // while (i < j) {
+        //     char temp = s[i];
+        //     s[i] = s[j];
+        //     s[j] = temp;
+        //     i++;
+        //     j--;
+        // }
 }
 }
